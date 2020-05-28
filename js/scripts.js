b352228f7038d37b9e6253d976302f0009667255
me <me@me.me.com>
2020-05-28 14:38:59 -0700

    add bootstrap and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,45 @@
 //business logic
 
 function Game(){
-  this.currentUser = "X";
+  this.currentPlayer = "X";
   this.winner = "";
 }
 
-Game.prototype.switchUser(){
-  if (this.currentUser === "X"){
-    this.currentUser = "Y"
+Game.prototype.switchPlayer(){
+  if (this.currentPlayer === "X"){
+    this.currentPlayer = "Y"
   }
   else {
-    this.currentUser = "X"
+    this.currentPlayer = "X"
   }
 }
 
 Game.prototype.determineWinner = function(){
   for(var i = 0; i <= 2; i++){
     if (board.boardState[i].every((val, i, arr) => val === arr[0] )){
-     this.winner = this.currentUser;
+     this.winner = this.currentPlayer;
       break;
     }
     if (board.boardState[0][i] === board.boardState[1][i] === board.boardState[2][i]){
-      this.winner = this.currentUser;
+      this.winner = this.currentPlayer;
       break;
     } 
   }
   if(board.boardState[0][0] === board.boardState[1][1] === board.boardState[2][2]){
-    this.winner = this.currentUser;
+    this.winner = this.currentPlayer;
   }
 
   if(board.boardState[0][2] === board.boardState[1][1] === board.boardState[2][0]){
-    this.winner = this.currentUser;
+    this.winner = this.currentPlayer;
   }
   
 }
 
 let game = new Game();
-function Move(x,y, currentUser){
+
+function Move(x,y){
   this.x = x;
   this.y = y;
-  this.currentUser = currentUser; 
 }
 
 function Board(){
@@ -56,7 +56,7 @@ Board.prototype.isAlreadyOccupied = function(move){
 
 Board.prototype.addOccupiedSpace = function(move){
   if(!board.isAlreadyOccupied){
-    this.boardState[move.y][move.x] = move.currentUser;
+    this.boardState[move.y][move.x] = game.currentPlayer;
   }
   //call these everytime a new space is occupied
   game.determineWinner();
@@ -65,10 +65,14 @@ Board.prototype.addOccupiedSpace = function(move){
     return;
   }
   game.switchUser();
-  
-
 }
 
+let board = new Board();
+
+function go(x,y){
+  let move = new Move(x,y)
+  board.addOccupiedSpace(move)
+}
 
 $(document).ready(function(){
 
