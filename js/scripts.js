a513894be1b269be5277baff301f9589315ca43f
me <me@me.me.com>
2020-05-28 14:13:02 -0700

    implement second spec of win condition function

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function Game(){
   this.currentUser = "X";
   this.winner = "";
 }
+
 Game.prototype.switchUser(){
   if (this.currentUser === "X"){
     this.currentUser = "Y"
@@ -13,6 +14,21 @@ Game.prototype.switchUser(){
   }
 }
 
+Game.prototype.isWinner = function(){
+  for(var i = 0; i <= 2; i++){
+    if (board.boardState[i].every((val, i, arr) => val === arr[0] )){
+      game.winner = currentUser;
+      break;
+    }
+    if (board.boardState[0][i] === board.boardState[1][i] === board.boardState[2][i]){
+      game.winner == currentUser;
+      break;
+    } 
+  }
+  
+}
+
+let game = new Game();
 function Move(x,y, currentUser){
   this.x = x;
   this.y = y;
@@ -32,10 +48,17 @@ Board.prototype.isAlreadyOccupied = function(move){
 }
 
 Board.prototype.addOccupiedSpace = function(move){
-  this.boardState[move.y][move.x] = move.currentUser; 
+  if(!board.isAlreadyOccupied){
+    this.boardState[move.y][move.x] = move.currentUser;
+  }
+  //call these everytime a new space is occupied
+  if (game.isWinner()){
+    return;
+  }
+  game.switchUser();
+  
+
 }
-
-
 
 
 $(document).ready(function(){
