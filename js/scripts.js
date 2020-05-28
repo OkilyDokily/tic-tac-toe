992090c27bcb52f581b69baac259b0b59d4ac9eb
me <me@me.me.com>
2020-05-28 13:26:59 -0700

    implement first three specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 //business logic
+
+function Game(){
+  this.currentUser = "X";
+  this.winner = "";
+}
+Game.prototype.switchUser(){
+  if (this.currentUser === "X"){
+    this.currentUser = "Y"
+  }
+  else {
+    this.currenUser = "X"
+  }
+}
+
 function Board(){
   this.boardState = [["","",""],["","",""],["","",""]];
 }
