eae5f85907c20cb0985a1a397ab9ea7d48509e53
me <me@me.me.com>
2020-06-02 15:18:12 -0700

    create algorithm to find two in a row

--- js/scripts.js
+++ js/scripts.js
@@ -130,6 +130,63 @@ function goComputerHard(){
       board.addOccupiedSpace(move);
     }
   }
+  if (turn === 3){
+
+  }
+}
+
+function isTwo() {
+  for (let i = 0; i <= 2; i++) {
+    if (board.boardState[i].filter(item => item === "X").length === 2 && board.boardState.some(item => item === "")){
+      let index = board.boardState[i].findIndex(item => item === "");
+      let move = new Move(index,i);
+      return {player:"X", emptySpace:move}
+      break;
+    }
+
+    if (board.boardState[i].filter(item => item === "O").length === 2 && board.boardState.some(item => item === "")){
+      let index = board.boardState[i].findIndex(item => item === "");
+      let move = new Move(index,i)
+      return {player:"O",emptySpace:move}
+      break;
+    }
+
+    if ([board.boardState[0][i], board.boardState[1][i], board.boardState[2][i]].filter(item => item === "X").length === 2 && [board.boardState[0][i], board.boardState[1][i], board.boardState[2][i]].some(item => item === "")){
+      let index = [board.boardState[0][i], board.boardState[1][i], board.boardState[2][i]].findIndex(item => item === "");
+      let move = new Move(i, index);
+      return {player:"X", emptySpace:move}
+    }
+    if ([board.boardState[0][i], board.boardState[1][i], board.boardState[2][i]].filter(item => item === "O").length === 2 && [board.boardState[0][i], board.boardState[1][i], board.boardState[2][i]].some(item => item === "")){
+      let index = [board.boardState[0][i], board.boardState[1][i], board.boardState[2][i]].findIndex(item => item === "");
+      let move = new Move(i, index)
+      return {player:"O", emptySpace:move}
+    }
+  }
+
+
+  if ([board.boardState[0][0], board.boardState[1][1], board.boardState[2][2]].filter(item => item === "X").length === 2 && [board.boardState[0][0], board.boardState[1][1], board.boardState[2][2]].some(item => item === "")){
+    let index = [board.boardState[0][0], board.boardState[1][1], board.boardState[2][2]].findIndex(item => item === "");
+    let move = new Move(index, index);
+    return {player:"X",emptySpace: move}
+  }
+
+  if ([board.boardState[0][0], board.boardState[1][1], board.boardState[2][2]].filter(item => item === "O").length === 2 && [board.boardState[0][0], board.boardState[1][1], board.boardState[2][2]].some(item => item === "")){
+    let index = [board.boardState[0][0], board.boardState[1][1], board.boardState[2][2]].findIndex(item => item === "");
+    let move = new Move(index, index);
+    return {player:"O",emptySpace: move}
+  }
+
+  if ([board.boardState[0][2], board.boardState[1][1], board.boardState[2][0]].filter(item => item ==="X").length === 2 && [board.boardState[0][2], board.boardState[1][1], board.boardState[2][0]].some(item => item === "")){
+    let index = [board.boardState[0][2], board.boardState[1][1], board.boardState[2][0]].findIndex(item => item === "");
+    let move = new Move(2 - index ,index)
+    return {player:"X",emptySpace:move}
+  }
+
+  if ([board.boardState[0][2], board.boardState[1][1], board.boardState[2][0]].filter(item => item ==="O").length === 2 && [board.boardState[0][2], board.boardState[1][1], board.boardState[2][0]].some(item => item === "")){
+    let index = [board.boardState[0][2], board.boardState[1][1], board.boardState[2][0]].findIndex(item => item === "");
+    let move = new Move(2 - index ,index)
+    return {player:"O",emptySpace:move}
+  }
 }
 
 
